d697c9c8359050376a93d4604471c80a3f892f1d
Bogibek_Matyaqubov <uzroot.pdp@gmail.com>
2022-02-28 21:16:49 +0500

    MainActivity.kt changed

--- app/src/main/java/dev/matyaqubov/pnotes/activity/MainActivity.kt
+++ app/src/main/java/dev/matyaqubov/pnotes/activity/MainActivity.kt
@@ -1,8 +1,7 @@
 package dev.matyaqubov.pnotes.activity
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -10,31 +9,28 @@ import dev.matyaqubov.pnotes.MyApplication
 import dev.matyaqubov.pnotes.R
 import dev.matyaqubov.pnotes.adapter.NoteAdapter
 import dev.matyaqubov.pnotes.helper.AddDialog
-import dev.matyaqubov.pnotes.manager.PrefManager
-import dev.matyaqubov.pnotes.model.Note
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
-    private lateinit var context: Context
 
-    companion object{
-        var notes=MyApplication.notes
-        var adapter=NoteAdapter(notes)
+    companion object {
+        var notes = MyApplication.notes
+        var adapter = NoteAdapter(notes)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        context=this
         initViews()
     }
 
     private fun initViews() {
-        recyclerView=findViewById(R.id.recyclerView)
-        recyclerView.layoutManager=GridLayoutManager(this,1)
-        recyclerView.adapter=adapter
-        val add=findViewById<FloatingActionButton>(R.id.b_add_note)
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = GridLayoutManager(this, 1)
+        recyclerView.adapter = adapter
+        val add = findViewById<FloatingActionButton>(R.id.b_add_note)
         add.setOnClickListener {
-            val dialog=AddDialog(this)
+            val dialog = AddDialog(this)
             dialog.show()
         }
     }
